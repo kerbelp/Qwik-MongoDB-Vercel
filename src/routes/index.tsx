db77f99ab4298fc44a71a87df173aef4540bfe37
Pavel Kerbel <kerbelp@gmail.com>
2023-05-28 15:11:21 +0300

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,102 +1,53 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import * as mongoDB from "mongodb";
 
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
+export class User {
+  constructor(
+    public id: number,
+    public name: string,
+    ) {}
+}
+
+export const collections: { users?: mongoDB.Collection } = {}
+
+export async function connectToDatabase(connection_string: string, db_name: string, collection_name: string) {
+
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(connection_string);
+
+  await client.connect();
+
+  const db: mongoDB.Db = client.db(db_name);
+
+  const appsCollection: mongoDB.Collection = db.collection(collection_name);
+
+  collections.users = appsCollection;
+}
+
+
+export const useGetUsers = routeLoader$(async (context) => {
+  if (collections.users === undefined) {
+    const connection_string = context.env.get('DB_CONN_STRING');
+    const db_name = context.env.get('DB_NAME');
+    const collection_name = context.env.get('COLLECTION_NAME');
+    if(connection_string === undefined || db_name === undefined || collection_name === undefined){
+      throw new Error('Environment variables missing (DB_CONN_STRING, DB_NAME, COLLECTION_NAME');
+    }
+    await connectToDatabase(connection_string, db_name, collection_name);
+  }
+
+  const data = await collections.users!.find({}, { projection: { '_id': 0 } }).toArray() as unknown as User[];
+  return data;
+});
 
 export default component$(() => {
+  const users = useGetUsers();
   return (
     <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{' '}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
+     {users.value.map((u)=> {
+        return (<div key={u.id}>id: {u.id}, name: {u.name}</div>)
+     })}
     </>
   );
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dotenv from 'dotenv'
+
+dotenv.config() // load env vars from .env
 
 export default defineConfig(() => {
   return {
